73800e0fa686e1f6c36f3404eab3889145f3d479
AnjalaD <anjaladilhara@gmail.com>
2024-07-13 22:09:02 +0530

    Add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NextUIProvider } from "@nextui-org/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             <NextUIProvider>{children}</NextUIProvider>
           </div>
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
